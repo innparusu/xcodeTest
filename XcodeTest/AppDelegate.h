a026afea8a4eb719a459b23f4fe53a180b3bd07c
innparusu <innparusu95@yahoo.co.jp>
2013-10-10 14:00:52 +0900

    Initial Commit

---
+++ XcodeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XcodeTest
+//
+//  Created by Tatsuki IHA on 2013/10/10.
+//  Copyright (c) 2013年 Tatsuki IHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XcodeTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XcodeTest
+//
+//  Created by Tatsuki IHA on 2013/10/10.
+//  Copyright (c) 2013年 Tatsuki IHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
